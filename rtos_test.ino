8b89f38b12dfdc9ff8f9a795d1d9d37883bc28e2
rohan-sonthalia <88440627+rohan-sonthalia@users.noreply.github.com>
2023-01-24 19:16:09 +0530

    Add files via upload

---
+++ rtos_test.ino
@@ -0,0 +1,49 @@
+#include <Arduino_FreeRTOS.h>
+
+TaskHandle_t TaskController_Handler;
+TaskHandle_t TaskButton_Handler;
+TaskHandle_t TaskOg_Handler;
+
+void TaskController();
+void TaskButton();
+void TaskOg();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  xTaskCreate(TaskController, "Joystick Input", 128, NULL, 1, &TaskController_Handler);
+  xTaskCreate(TaskButton, "Button Interrupt", 128, NULL, 1, &TaskButton_Handler);
+  xTaskCreate(TaskOg, "Original Task", 128, NULL, 1, &TaskOg_Handler);
+
+  // vTaskStartScheduler();
+}
+
+void TaskOg(){
+  Serial.println("Original Task");
+  vTaskDelay( 500 / portTICK_PERIOD_MS );
+}
+
+void TaskController(){
+  if(Serial.available()){
+    if(Serial.read()=="a"){
+      vTaskSuspend(TaskOg_Handler);
+      Serial.println("Moving to Button Oriented Task");
+      vTaskResume(TaskButton_Handler);
+    }
+  }
+  
+}
+
+void TaskButton(){
+  int t = millis();
+  Serial.println("Button Oriented Task");
+  if(millis()-t == 5000){
+    vTaskResume(TaskOg_Handler);
+    vTaskSuspend(TaskButton_Handler);
+  }
+}
+
+void loop() {
+  // Keep Empty
+}
